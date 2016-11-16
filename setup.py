00c795f1b2eb4706dc796a8684ec5ed9cb900f90
vatregubov <vatregubov@sberned.ru>
2016-11-16 17:49:18 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [
 ]
 
 setup(name='djaio',
-      version='0.0.8',
+      version='0.0.9',
       description='Djaio - Django-inspired AsyncIO web framework',
       author='Vadim Tregubov',
       author_email='vatregubov@sberned.ru',
