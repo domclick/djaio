0b74656764261db15191c5946d2cc372db99bba7
pvgorbunov <pvgorbunov@domclick.ru>
2017-04-12 16:48:44 +0300

    tests

--- djaio/core/views.py
+++ djaio/core/views.py
@@ -149,7 +149,7 @@ class MobileApiJsonView(JsonView):
             raise web.HTTPMethodNotAllowed
 
         response = {
-            'code': 0,
+            'code': 200,
             'data': {}
         }
 
