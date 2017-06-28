0a5860363b0abd6748d78aaa4968188c6298b8dd
Черемушкин Вячеслав <vvcheremushkin@domclcik.ru>
2017-06-28 12:12:53 +0300

    db: remove commit

--- djaio/ext/db.py
+++ djaio/ext/db.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import warnings
+from typing import List, Dict, Union, Optional
 
 import aiopg
-from psycopg2.extras import DictCursor
-from typing import List, Dict
+from psycopg2.extras import DictCursor, DictRow
 
 
 class DB:
@@ -35,11 +35,6 @@ class DB:
     async def execute(self, db_name: str, query: str, values: List, _type: str):
         """
         Execute SQL query in connection pool
-        :param db_name:
-        :param query:
-        :param values:
-        :param _type:
-        :return:
         """
         warnings.warn("Use single methods!", DeprecationWarning)
 
@@ -59,69 +54,66 @@ class DB:
         async with pool.acquire() as conn:
             async with conn.cursor(cursor_factory=DictCursor) as cursor:
                 await cursor.execute(query, values)
-
                 if _type == 'select':
                     data = await cursor.fetchall()
                 else:
-                    cursor.connection.commit()
                     data = cursor.rowcount
         return data
 
-    async def select(self, query: str, values: List, db_name: str = 'default'):
-        return await self._execute(query=query, values=values, _type='select',
-                                   db_name=db_name)
+    async def select(self, query: str, values: Union[List, Dict],
+                     db_name: str = 'default') -> List[DictRow]:
+        return await self._select(query=query, values=values, db_name=db_name)
 
-    async def insert(self, query: str, values: List,
+    async def first(self, query: str, values: Union[List, Dict],
+                    db_name: str = 'default') -> Optional[DictRow]:
+        return await self._first(query=query, values=values, db_name=db_name)
+
+    async def insert(self, query: str, values: Union[List, Dict],
                      db_name: str = 'default', returning: bool = False):
-        return await self._execute(query=query, values=values, _type='insert',
-                                   db_name=db_name, returning=returning)
+        return await self._execute(query=query, values=values, db_name=db_name, returning=returning)
 
-    async def update(self, query: str, values: List,
+    async def update(self, query: str, values: Union[List, Dict],
                      db_name: str = 'default', returning: bool = False):
-        return await self._execute(query=query, values=values, _type='update',
-                                   db_name=db_name, returning=returning)
+        return await self._execute(query=query, values=values, db_name=db_name, returning=returning)
 
-    async def delete(self, query: str, values: List, db_name: str = 'default'):
-        return await self._execute(query=query, values=values, _type='delete',
-                                   db_name=db_name)
+    async def delete(self, query: str, values: Union[List, Dict], db_name: str = 'default'):
+        return await self._execute(query=query, values=values, db_name=db_name)
 
-    async def _execute(self, query: str, values: List, _type: str,
-                       db_name: str = 'default', returning: bool = False):
-        """
-        Execute SQL query in connection pool
-        :param query:
-        :param values:
-        :param _type:
-        :param db_name:
-        :param returning:
-        :return:
-        """
-
-        if _type not in ('select', 'insert', 'update', 'delete'):
-            raise RuntimeError(
-                'Wrong request type {}'.format(_type)
-            )
-        if not self.dbs[db_name]['master']:
-            raise RuntimeError(
-                'db {} master is not initialized'.format(db_name)
-            )
-
+    async def _execute(self, query: str, values: Union[List, Dict], db_name: str = 'default',
+                       returning: bool = False):
         pool = self.dbs[db_name]['master']
-        if _type == 'select' and 'slave' in self.dbs[db_name]:
-            pool = self.dbs[db_name]['slave']
+        if pool is None:
+            raise RuntimeError('db {} master is not initialized'.format(db_name))
 
         async with pool.acquire() as conn:
             async with conn.cursor(cursor_factory=DictCursor) as cursor:
                 await cursor.execute(query, values)
-                if _type == 'select':
-                    return await cursor.fetchall()
+                if returning:
+                    return await cursor.fetchone()
                 else:
-                    cursor.connection.commit()
-                    if returning:
-                        data = await cursor.fetchone()
-                    else:
-                        data = cursor.rowcount
-        return data
+                    return cursor.rowcount
+
+    async def _select(self, query: str, values: Union[List, Dict], db_name: str = 'default'):
+        dbs = self.dbs[db_name]
+        pool = dbs.get('slave') or dbs.get('master')
+        if pool is None:
+            raise RuntimeError('db {} master is not initialized'.format(db_name))
+
+        async with pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=DictCursor) as cursor:
+                await cursor.execute(query, values)
+                return await cursor.fetchall()
+
+    async def _first(self, query: str, values: Union[List, Dict], db_name: str = 'default'):
+        dbs = self.dbs[db_name]
+        pool = dbs.get('slave') or dbs.get('master')
+        if pool is None:
+            raise RuntimeError('db {} master is not initialized'.format(db_name))
+
+        async with pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=DictCursor) as cursor:
+                await cursor.execute(query, values)
+                return await cursor.fetchone()
 
 
 def setup(app):
